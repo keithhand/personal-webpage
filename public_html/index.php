234fcd4027ddb06148fa48c9857f8f7c5faf9ce1
keith.hand <rkeithhand@gmail.com>
2019-04-17 23:20:32 -0500

    scroll doesn't work so am commenting atm

--- public_html/index.php
+++ public_html/index.php
@@ -397,21 +397,33 @@
         e.preventDefault();
         $("#sidebar-wrapper").toggleClass("active");
     });
+
+
+
+
     // Scrolls to the selected menu item on the page
-    $(function() {
-        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
+    // $(function() {
+    //     $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+    //         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             if (target.length) {
+    //                 $('html,body').animate({
+    //                     scrollTop: target.offset().top
+    //                 }, 1000);
+    //                 return false;
+    //             }
+    //         }
+    //     });
+    // });
+
+
+
+
+
+
+
+
     //#to-top button appears after scrolling
     var fixed = false;
     $(document).scroll(function() {
