5db6acc0511910d5be438e161f4fb72ade314559
keith.hand <keith@hand.technology>
2019-06-16 20:19:22 -0500

    [setup-new-files] * initial files * added dataloader variables * add gitignore

---
+++ data.py
@@ -0,0 +1,58 @@
+title="Software Developer"
+email="rkeithhand@gmail.com"
+phone="(405) 574 5813"
+banner="Hello, my name is Keith, and I like to write code!"
+description="I enjoy taking on any project, big or small. Whether you need a personal webpage, you have the next big AAA iOS title, or you need someone to manage your webserver, I want to be the guy to help make your dreams become a reality."
+
+languages=[
+    {
+        'title': "HTML",
+        'icon': "fa-html5",
+        'iconcat': "fab"
+    },
+    {
+        'title': "PHP",
+        'icon': "fa-php",
+        'iconcat': "fab"
+    },
+    {
+        'title': "JavaScript",
+        'icon': "fa-js-square",
+        'iconcat': "fab"
+    },
+    {
+        'title': "CSS",
+        'icon': "fa-css3-alt",
+        'iconcat': "fab"
+    }
+]
+
+projects=[
+    {
+        'title': "UCO Maps",
+        'desc': "A web application to determine directions between building at the University of Central Oklahoma.",
+        'lang': "Ruby",
+        'git': "http://github.com/keithhand/se-project",
+        'web': "http://uptownok.com/"
+    },
+    {
+        'title': "Ant Facts",
+        'desc': "A slack bot that give random facts about ants.",
+        'lang': "Python",
+        'git': "http://github.com/keithhand/ant-facts"
+    },
+    {
+        'title': "command.here()",
+        'desc': "A web portal to mimic a shell environment in any browser.",
+        'lang': "undecided",
+        'git': "http://github.com/keithhand/personal-portal",
+        'web': ""
+    },
+    {
+        'title': "Realm Tracker Extended",
+        'desc': "A firefox extension that gives added functionality to realmtracker.com.",
+        'lang': "JavaScript",
+        'git': "http://github.com/keithhand/realm-tracker-extended",
+        'web': "https://addons.mozilla.org/en-US/firefox/addon/realm-tracker-extended/"
+    },
+]
